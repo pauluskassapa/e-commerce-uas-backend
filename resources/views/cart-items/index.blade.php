7ed5c30d83e02c2673b5b2f26a995fb163d88a16
strelicas <danielsutanto599@gmail.com>
2026-06-17 18:53:39 +0700

    cart item awal

--- resources/views/cart-items/index.blade.php
+++ resources/views/cart-items/index.blade.php
@@ -1,27 +1,75 @@
 @extends('layouts.app')
 
 @section('content')
-    <h2>Cart Items</h2>
-    <p>TODO: isi detail item cart yang terhubung dengan produk.</p>
+<div class="container">
+    <h2>Shopping Cart</h2>
 
-    <table border="1" cellpadding="6">
-        <tr>
-            <th>ID</th>
-            <th>Cart</th>
-            <th>Product</th>
-            <th>Qty</th>
-            <th>Price</th>
-        </tr>
-        @forelse ($items as $item)
-            <tr>
-                <td>{{ $item->id }}</td>
-                <td>{{ $item->cart_id }}</td>
-                <td>{{ $item->product?->name ?? '-' }}</td>
-                <td>{{ $item->quantity }}</td>
-                <td>{{ $item->price }}</td>
-            </tr>
-        @empty
-            <tr><td colspan="5">Belum ada item cart.</td></tr>
-        @endforelse
-    </table>
+    @php $grandTotal = 0; @endphp
+
+    @forelse($items as $item)
+
+        @php
+            $subtotal = $item->quantity * $item->price;
+            $grandTotal += $subtotal;
+        @endphp
+
+        <div class="cart-item">
+
+            <div class="cart-image">
+                <img src="{{ $item->product->image ?? 'https://via.placeholder.com/150' }}"
+                     width="120">
+            </div>
+
+            <div class="cart-info">
+                <h3>{{ $item->product->name }}</h3>
+
+                <p>
+                    {{ $item->product->description }}
+                </p>
+
+                <p>
+                    Harga:
+                    <strong>
+                        Rp {{ number_format($item->price,0,',','.') }}
+                    </strong>
+                </p>
+            </div>
+
+            <div class="cart-qty">
+
+                <form action="{{ route('cart.decrease',$item->product_id) }}"
+                      method="POST">
+                    @csrf
+                    <button type="submit">-</button>
+                </form>
+
+                <span>{{ $item->quantity }}</span>
+
+                <form action="{{ route('cart.increase',$item->product_id) }}"
+                      method="POST">
+                    @csrf
+                    <button type="submit">+</button>
+                </form>
+
+            </div>
+
+            <div class="cart-total">
+                Rp {{ number_format($subtotal,0,',','.') }}
+            </div>
+
+        </div>
+
+    @empty
+        <p>Cart masih kosong.</p>
+    @endforelse
+
+    <hr>
+
+    <div class="grand-total">
+        <h3>
+            Total:
+            Rp {{ number_format($grandTotal,0,',','.') }}
+        </h3>
+    </div>
+</div>
 @endsection
