882bd617d67a5a47f0efd1cdb9160f186fa2432c
Herman Sim <paulussuherman1@gmail.com>
2026-06-23 16:56:35 +0700

    ubah di order pakai library baru

--- app/Models/Order.php
+++ app/Models/Order.php
@@ -3,6 +3,8 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Order extends Model
 {
@@ -12,12 +14,12 @@ class Order extends Model
         'status'
     ];
 
-    public function user()
+    public function user(): BelongsTo
     {
         return $this->belongsTo(User::class);
     }
 
-    public function items()
+    public function items(): HasMany
     {
         return $this->hasMany(OrderItem::class);
     }
