d2a9d0b8c7f4da540e742987ece25e4d9df059d1
DefAlgi <defrigoalgian01@gmail.com>
2026-06-23 22:35:55 +0700

    membuat tabel order

--- database/migrations/2026_06_21_155036_create_orders_table.php
+++ database/migrations/2026_06_21_155036_create_orders_table.php
@@ -4,38 +4,27 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
-    /**
-     * Run the migrations.
-     */
+return new class extends Migration {
     public function up(): void
     {
         Schema::create('orders', function (Blueprint $table) {
+            $table->id();
 
-        $table->id();
+            $table->foreignId('user_id')->constrained()->cascadeOnDelete();
 
-        $table->foreignId('user_id')
-              ->constrained()
-              ->cascadeOnDelete();
+            $table->decimal('total_amount', 12, 2);
 
-        $table->decimal('total_price',10,2);
+            $table->string('status')->default('pending');
+            // pending, paid, cancelled
 
-        $table->enum('status',[
-            'pending',
-            'paid',
-            'shipped',
-            'completed',
-            'cancelled'
-        ])->default('pending');
+            $table->text('notes')->nullable();
 
-        $table->timestamps();
-    });
-}
+            $table->timestamp('paid_at')->nullable();
 
-    /**
-     * Reverse the migrations.
-     */
+            $table->timestamps();
+        });
+    }
+
     public function down(): void
     {
         Schema::dropIfExists('orders');
